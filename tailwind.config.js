32421489013eb0de989cba388dd2d12bbf4519b6
Jose Corredor <jose96corrza@gmail.com>
2023-09-25 23:47:04 -0500

    Initial commit :rocket:

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  darkMode: 'class',
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    colors: {
+      'dark-blue': '#071952',
+      'teal': '#0B666A',
+      'semi-mint': '#35A29F',
+      'mint': '#97FEED'
+    },
+    fontFamily: {
+      sans: ['Graphik', 'sans-serif'],
+      serif: ['Merriweather', 'serif'],
+    },
+    extend: {},
+  },
+  plugins: [],
+}
+
